342325c14832271a7a029853eb992e0e922e5045
Karine <karinedolar@gmail.com>
2026-09-02 23:28:49 -0300

    Ignore local database file

--- ver_db.py
+++
@@ -1,8 +0,0 @@
-from app import app
-from app.models import Usuario
-
-with app.app_context():
-    usuarios = Usuario.query.all()
-
-    for usuario in usuarios:
-        print(usuario.id, usuario.nome, usuario.sobrenome, usuario.email, usuario.senha)
