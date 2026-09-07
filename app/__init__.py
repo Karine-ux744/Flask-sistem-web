a3e2560a76947ba33ec62069d06f3f2bdcb9198c
Karine <karinedolar@gmail.com>
2026-09-07 14:38:19 -0300

    chore: update requirements with python-dotenv

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,16 @@ from flask import Flask
 from app.database import database
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+import os
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 database.init_app(app)
-app.config["SECRET_KEY"] = "6e734149ec202e64c8ca"
 login_manager = LoginManager(app)
 bcrypt = Bcrypt(app)
 login_manager.login_view="login"
 
+load_dotenv()
+secret_key = os.getenv("app.config['SECRET_KEY']")
 from app import routes
