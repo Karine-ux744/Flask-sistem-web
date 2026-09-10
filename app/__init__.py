a6baf241dfa613597ab59bdd413586b7143d96ab
Karine <karinedolar@gmail.com>
2026-09-09 21:02:58 -0300

    Configure production database connection and build steps for Render

--- app/__init__.py
+++ app/__init__.py
@@ -6,12 +6,17 @@ import os
 from dotenv import load_dotenv
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
-database.init_app(app)
 login_manager = LoginManager(app)
 bcrypt = Bcrypt(app)
 login_manager.login_view="login"
 
 load_dotenv()
 secret_key = os.getenv("app.config['SECRET_KEY']")
+url_database = os.environ.get('DATABASE_URL')
+if url_database and url_database.startswith("postgres://"):
+  url_database = url_database.replace("postgres://", "postgresql://", 1)
+app.config['SQLALCHEMY_DATABASE_DATABASE_URI'] = url_database or 'sqlite:///local.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+database.init_app(app)
+
 from app import routes
--- criar_db.py
+++ criar_db.py
@@ -2,5 +2,10 @@ from app.database import database
 from app import app 
 from app.models import Usuario
 
+url_database = os.environ.get('DATABASE_URL')
+if url_database and url_database.startswith("postgres://"):
+  url_database = url_database.replace("postgres://", "postgresql://", 1)
+app.config['SQLALCHEMY_DATABASE_DATABASE_URI'] = url_database or 'sqlite:///local.db'
+
 with app.app_context():
   database.create_all()
